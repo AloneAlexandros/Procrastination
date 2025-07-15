9f0506b29195ff4b1ff087cb8042584e3b279046
Alexandros Samonakis <98485800+AloneAlexandros@users.noreply.github.com>
2025-07-16 00:19:27 +0300

    added a watering can
    3d model and code and all

--- Assets/Scripts/WaterPlant.cs
+++ Assets/Scripts/WaterPlant.cs
@@ -5,5 +5,6 @@ public class WaterPlant : MonoBehaviour
     public void WaterThePlant()
     {
         Variables.PlantsWatered++;
+        GameObject.Find("wateringcan").GetComponent<Animator>().SetTrigger("watering");
     }
 }
